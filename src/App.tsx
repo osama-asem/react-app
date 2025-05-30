8198b2dd296caef920c420576f8c8cf240dd3a7f
osama-asem <osamaasem161@gmail.com>
2025-05-31 01:27:54 +0300

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import NextPage from "./NextPage";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0);
 
