eade917bf246a554d15562a15b30bb784237644e
osama-asem <osamaasem161@gmail.com>
2025-05-30 20:10:30 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base:"https://osama-asem.github.io/react-app"
+  base:"react-app",
+  build:{
+    outDir:'build'
+  }
 })
